b49fbd06387d6499ccda3643f79df1bdd9f2d29f
jawahar jovah <jawaharjovah2003@gmail.com>
2025-12-29 14:00:09 +0530

    updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st,pandas as pd,main
-from main import wanted_data_gather,get_channel_info
+from main import get_channel_info
 import numpy as np
 
 st.set_page_config("youtube harvester",layout="wide")
@@ -45,7 +45,7 @@ if search:
                 st.success(channel)
                 channel_list.add(channel)
             else:
-                st.warning("channel does not exist!")
+                st.warning("channel does not exist or channel activity may not be irregular !")
                 st.session_state.selected_channel = None
 
 if len(channel_list)<=1:
@@ -59,28 +59,27 @@ if len(channel_list)<=1:
                 index=playlist_names.index(st.session_state.selected_playlist) if st.session_state.selected_playlist in playlist_names else 0
                 )
             video_count = c2.number_input("videos from playlist",max_value=10 if main.playlistitems_results <= 3 else main.playlistitems_results,min_value=3)
-            st.write()
-            st.write("-"*50)
+            
+            st.divider()
             st.markdown(":red[Table] : channel")
             data = main.DATA_POOL["channel_details"]
-            df = pd.DataFrame(data,columns = data.keys())
+            df = pd.DataFrame(data,columns = list(data.keys()))
             df.drop("channel_description",axis=1,inplace=True)
             st.table(df)
 
         if any(df := pd.DataFrame(main.DATA_POOL['playlist_details'],columns=main.DATA_POOL["playlist_details"].keys(),index=[i for i in range(0,len(main.DATA_POOL["playlist_details"]["playlist_name"]))])):
             st.write()
-            st.write("-"*50)
+            st.divider()
             st.markdown(":red[Table] : playlists")
             st.table(df)
         else:
             st.write()
-            st.write("-"*50)
+            st.divider()
             st.markdown(":red[Table] : playlists")
             st.error("No playlists found !")
         if selected_playlist:
             temp_map = dict(zip(main.DATA_POOL["playlist_details"]["playlist_name"],main.DATA_POOL["playlist_details"]["playlist_id"]))
             main.get_playlistItems(temp_map[selected_playlist],count = video_count)
-            
             item_names = main.DATA_POOL['playlist_items'].get("item_name", [])
             selected_video = c3.selectbox("Select video",options=item_names,
             index=item_names.index(st.session_state.selected_video) if st.session_state.selected_video in item_names else 0 if item_names else None,
@@ -90,24 +89,30 @@ if len(channel_list)<=1:
             data = main.DATA_POOL["playlist_items"]
             if any(df := pd.DataFrame(data,columns=data.keys())):
                 st.write()
-                st.write("-"*50)
-                st.markdown(":red[Table] : Playlist Items")
+                st.divider()
+                st.markdown(f":red[Table] : \"{selected_playlist}\" items")
                 st.table(df)
             else:
                 st.write()
-                st.write("-"*50)
-                st.markdown(":red[Table] : Playlist Items")
+                st.divider()
+                st.markdown(f":red[Table] : \"{selected_playlist}\" items")
                 st.error("No playlist items found !")
             pdet = main.DATA_POOL["playlist_items"]
             temp_video_map = dict(zip(pdet['item_name'],pdet['item_id']))
             if selected_video:
                 main.get_video_details(temp_video_map[selected_video])
-                data = main.DATA_POOL["video_details"]
-                data.pop("video_description")
-                st.write()
-                st.write("-"*50)
-                st.write(":red[Table] : video details")
-                st.table(data)
+                
+                total_videos = main.get_every_video_from_playlists(channel=channel)
+                # st.table(total_videos)
+                sc_data = main.DATA_POOL["video_details"]
+                sc_data.pop("video_description")
+                # wanted_data_gather(selected_video)
+                # data = main.DATA_POOL["extra_det"]
+                st.divider()
+                st.write(f":red[Table] : \"{selected_video} \" details")
+                st.table(sc_data)
+                # main.get_video_details('rzX5m2UZXFo')
+                # st.table(main.DATA_POOL['video_details'])
                 # if any(df := pd.DataFrame(data,columns=data.keys(),index=[0])):
                 #     st.write()
                 #     st.write("-"*50)
@@ -116,12 +121,14 @@ if len(channel_list)<=1:
                 #     st.write()
                 #     st.write("-"*50)
                 #     st.warning("no video details found")
-        st.write(main.DATA_POOL)
+        # st.write(main.DATA_POOL)
+
+            main.get_every_video_in_playlist(temp_map[selected_playlist])
 
 elif len(channel_list) > 1:
+    
     channel_details_dict = {}
     playlist_details_dict = {}
-    data = dict()
     for channel in channel_list:
         channel_id = main.get_channel_id_by_handle(channel) if (channel[0] == '@') else main.get_channel_id_by_username(channel)
         if get_channel_info(channel_id):
@@ -129,27 +136,100 @@ elif len(channel_list) > 1:
             pl_det = main.DATA_POOL["playlist_details"]
             channel_details_dict[channel] = ch_det
             playlist_details_dict[channel] = pl_det
-    # st.write(channel_details_dict)
-    # st.write(playlist_details_dict)
-    df1 = pd.DataFrame(channel_details_dict[list(channel_list)[0]])
-    st.table(df1)
+    ch_det_df = pd.DataFrame(channel_details_dict[list(channel_list)[0]])
+    pl_det_df = pd.DataFrame(playlist_details_dict[list(channel_list)[0]])
+
+    for ch_no in range(1,len(channel_list)):
+        df2 = pd.DataFrame(channel_details_dict[list(channel_list)[ch_no]])
+        df4 = pd.DataFrame(playlist_details_dict[list(channel_list)[ch_no]])
+
+        ch_det_df = ch_det_df.merge(df2,on=list(ch_det_df.columns),how="outer")
+        pl_det_df = pl_det_df.merge(df4,on=list(pl_det_df.columns),how="outer")
+
+    else:
+        ch_det_df.drop("channel_description",axis=1,inplace=True)
+        st.table(ch_det_df)
+        st.table(pl_det_df)
     
-    # THERE IS ERROR OCCURING ABIUT THE ARRAY NOT BEING SAME LENGTH
-    # THERE IS ERROR OCCURING ABIUT THE ARRAY NOT BEING SAME LENGTH
-    # THERE IS ERROR OCCURING ABIUT THE ARRAY NOT BEING SAME LENGTH
-    # THERE IS ERROR OCCURING ABIUT THE ARRAY NOT BEING SAME LENGTH
-    # THERE IS ERROR OCCURING ABIUT THE ARRAY NOT BEING SAME LENGTH
-    # THERE IS ERROR OCCURING ABIUT THE ARRAY NOT BEING SAME LENGTH
-    # THERE IS ERROR OCCURING ABIUT THE ARRAY NOT BEING SAME LENGTH
-
-    df2 = pd.DataFrame(channel_details_dict[list(channel_list)[1]])
-    # st.table(df2)
-    # st.table(df1.merge(df2,on=list(df.columns),how="outer"))
-    # st.write(data)
-    if data:
-        df1 = pd.DataFrame(data[list(data.keys())[0]],columns=data[list(data.keys())[0]].keys())
-        # df2 = pd.DataFrame(data[list(data.keys())[1]],columns=data[list(data.keys())[1]].keys())
-        # df1.merge(df2,on="")
-        st.table(df1)
+else:
+    pass
 
 
+x = np.random.randint(20,100,20).reshape(20,1)
+y = np.random.randint(20,100,20).reshape(20,1)
+
+# dt = pd.DataFrame(np.hstack([x,y]),columns=("row","col"),)
+# dt = pd.DataFrame({'row':[0,255],'col':[30,200]})
+# st.table(dt)
+# st.bar_chart(dt,x='row',y='col')
+
+# import plotly.figure_factory as ff
+# ff.create_
+# st.plotly_chart()
+
+if len(channel_list)>1:
+    import plotly.figure_factory as ff
+    details = {"ch_name":[],"view_count":[],"sub_count":[],"vid_count":[]}
+    st.divider()
+    div1,div2,div3 = st.columns([0.3,0.3,0.3])
+    for channel in channel_list:
+        if get_channel_info(main.get_channel_id_by_username(channel)):
+            ch_grh = main.DATA_POOL["channel_details"]
+            details["ch_name"].append(ch_grh["channel_name"][0])
+            details["view_count"].append(int(ch_grh["channel_views"][0]))
+            details["sub_count"].append(int(ch_grh["channel_subscriberCount"][0]))
+            details["vid_count"].append(int(ch_grh["channel_videos"][0]))
+
+    dat = pd.DataFrame(details)
+    # st.table(dat)
+    # st.write(dat)
+    div1.bar_chart(dat,x="ch_name",y="sub_count",x_label="channels",y_label="subscribers")
+    div2.bar_chart(dat,x="ch_name",y="vid_count",x_label="channels",y_label="videos")
+    div3.bar_chart(dat,x="ch_name",y="view_count",x_label="channels",y_label="views")
+
+    st.divider()
+    st.line_chart(dat,x="ch_name",y="sub_count",)
+    # fig = ff.create_distplot([dat["vid_count"],dat["view_count"],dat["sub_count"]],group_labels=dat["ch_name"],bin_size=10)
+    fig = ff.create_2d_density(x=dat["view_count"],y=dat["sub_count"],title="Relation between views and subs")
+    fig1 = ff.create_2d_density(x=dat["view_count"],y=dat["vid_count"],title="Relation between views and videos")
+    fig2 = ff.create_2d_density(x=dat["sub_count"],y=dat["vid_count"],title="Relation between subs and videos")
+    st.plotly_chart(fig)
+    st.plotly_chart(fig1)
+    st.plotly_chart(fig2)
+elif len(channel_list)<=1 and len(channel_list)!=0:
+    
+    details = {"ch_name":[],"view_count":[],"sub_count":[],"vid_count":[]}
+    st.divider()
+    div1,div2,div3 = st.columns([0.3,0.3,0.3])
+    for channel in channel_list:
+        if get_channel_info(main.get_channel_id_by_username(channel)):
+            ch_grh = main.DATA_POOL["channel_details"]
+            
+            details["ch_name"].append(ch_grh["channel_name"][0])
+            details["view_count"].append(int(ch_grh["channel_views"][0]))
+            details["sub_count"].append(int(ch_grh["channel_subscriberCount"][0]))
+            details["vid_count"].append(int(ch_grh["channel_videos"][0]))
+
+    dat = pd.DataFrame(details)
+    # st.table(dat)
+    # st.write(dat)
+    div1.bar_chart(dat,x="ch_name",y="sub_count",x_label="channel",y_label="subscribers")
+    div2.bar_chart(dat,x="ch_name",y="vid_count",x_label="channel",y_label="videos")
+    div3.bar_chart(dat,x="ch_name",y="view_count",x_label="channel",y_label="views")
+    # video_data = pd.DataFrame(sc_data)
+    # for pl_item in main.DATA_POOL["playlist_items"]["item_id"]:
+    #     temp_sc = pd.DataFrame(main.get_video_details(pl_item))
+    #     st.table(temp_sc)
+    #     video_data.merge(temp_sc,on="video_id",how="outer")
+    # st.table(video_data)
+
+
+
+
+
+
+
+#TODO: add additional functionality to the get_video_details() so it gets the details of all the videos not only one
+
+#TODO: for single channel display likes,view count,dislikes.
+#TODO: also conncet to the database
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from googleapiclient.discovery import build
 from dotenv import load_dotenv
 import os
 import streamlit as st
+import pandas as pd
 
 load_dotenv()
 key = os.getenv("API_KEY")
@@ -62,37 +63,6 @@ def get_video_id_by_name(n):
         for res in response["items"]:
             return res['id']['videoId']
 
-def wanted_data_gather(search):
-    video_id = get_video_id_by_name(search)
-
-    youtube = build('youtube', 'v3', developerKey=key)
-
-    video_response = youtube.videos().list(
-        part='snippet,statistics',
-        id=video_id
-    ).execute()
-    if video_response['items']:
-        video = video_response['items'][0]
-        DATA_POOL["**Title**"] = video['snippet']['title']
-        DATA_POOL["**Channel**"] = video['snippet']['channelTitle']
-        DATA_POOL["**Views**"]  = video['statistics']['viewCount']
-        DATA_POOL["**Likes**"] = video['statistics'].get('likeCount', 'Hidden')
-        DATA_POOL["**Comments**"] = video['statistics'].get('commentCount', 'Disabled')
-
-    comments_response = youtube.commentThreads().list(
-        part='snippet',
-        videoId=video_id,
-        maxResults=5
-    ).execute()
-
-    st.markdown("\nTop Comments:")
-    text = ""
-    for item in comments_response['items']:
-        comment = item['snippet']['topLevelComment']['snippet']
-        # st.write(f"- {comment['authorDisplayName']}: {comment['textDisplay'][:80]}...")
-        text+=(comment['authorDisplayName']+"-"+comment["textDisplay"][:30]+"..."+"  \n   ")
-    DATA_POOL["comment text"] = text
-
 def get_playlists(ch):
     """ for getting playlists"""
     count = 10
@@ -143,8 +113,9 @@ def get_playlistItems(playlist_id,count = 3):
     # st.write(response)
     playlistitems_results = response['pageInfo']['totalResults']
     if response['pageInfo']['totalResults'] >= count:
+        
         [
-        (playlist_items['item_name'].append(response["items"][i]["snippet"]["title"]),
+        (playlist_items['item_name'].append(response["items"][i]["snippet"]["title"] ),
         playlist_items['item_id'].append(response["items"][i]["contentDetails"]["videoId"]),
         playlist_items['item_date'].append(response["items"][i]["contentDetails"]["videoPublishedAt"] if ("videoPubilshedAt" in response['items'][i]['contentDetails']) else None),
         playlist_items['item_status'].append(response['items'][i]['status']["privacyStatus"]) )
@@ -152,7 +123,7 @@ def get_playlistItems(playlist_id,count = 3):
         ]
     else:
         [
-        (playlist_items['item_name'].append(response["items"][i]["snippet"]["title"]),
+        (playlist_items['item_name'].append(response["items"][i]["snippet"]["title"] ),
         playlist_items['item_id'].append(response["items"][i]["contentDetails"]["videoId"]),
         playlist_items['item_date'].append(response["items"][i]["contentDetails"]["videoPublishedAt"] if ("videoPubilshedAt" in response['items'][i]['contentDetails']) else None),
         playlist_items['item_status'].append(response['items'][i]['status']["privacyStatus"]) )
@@ -180,9 +151,9 @@ def get_video_details(video_id):
         "video_description" : [],
         "video_views" : [],
         "video_likes" : [],
-        # "video_dislikes" : [],
         "video_duration" : [],
         'video_comments_count' : [],
+        'video_commentText': [],
         "video_caption_status" : [],
         "video_thumbnail" : []
     }
@@ -192,31 +163,58 @@ def get_video_details(video_id):
         id = video_id
     ).execute()
     # st.write(response)
-    if response['items']:
-        video_details["video_name"]          = response["items"][0]["snippet"]['title'] if ("title" in  response["items"][0]['snippet'].keys()) else None
-        video_details["video_id"]            = response['items'][0]['id'] if ("items" in  response["items"]) else None
-        video_details["video_date"]          = response['items'][0]['snippet']['publishedAt'] if ("publishedAt" in  response["items"][0]['snippet'].keys()) else None
-        video_details["video_description"]   = response['items'][0]['snippet']['description'] if ("description" in  response["items"][0]['snippet'].keys()) else None
-        video_details["video_views"]         = response["items"][0]['statistics']['viewCount'] if ("viewCount" in  response["items"][0]['statistics'].keys()) else None
-        video_details["video_likes"]         = response["items"][0]['statistics']['likeCount'] if ("likeCount" in  response["items"][0]['statistics'].keys()) else None
-        # video_details["video_dislikes"]    = response[][0][][]
-        video_details["video_duration"]      = response["items"][0]['contentDetails']['duration'] if ("duration" in  response["items"][0]['contentDetails'].keys()) else None
-        video_details["video_comments_count"]= response['items'][0]['statistics']["commentCounts"] if ("commentCounts" in  response["items"][0]['statistics'].keys()) else None
-        video_details["video_caption_status"]= response["items"][0]['contentDetails']['caption'] if ("caption" in  response["items"][0]['contentDetails'].keys()) else None
-        video_details["video_thumbnail"]     = response["items"][0]["snippet"]["thumbnails"]["standard"]["url"] if ("thumbnails" in  response["items"][0]['snippet'].keys()) else None
+    
+        # extra_det["Likes"] = video['statistics'].get('likeCount', 'Hidden')
+        # extra_det["Comments"] = video['statistics'].get('commentCount', 'Disabled')
+    # assert response['items'] == None,"There are no items"
+    video = response['items'][0] if any(response['items']) else None
+    if video:
+        video_details["video_name"]          .append(video["snippet"]['title'] if ("title" in  response["items"][0]['snippet'].keys()) else None)
+        video_details["video_id"]            .append(video['id'])#if ("items" in  response["items"]) else None)
+        video_details["video_date"]          .append(video['snippet']['publishedAt'] if ("publishedAt" in  response["items"][0]['snippet'].keys()) else None)
+        video_details["video_description"]   .append(video['snippet']['description'] if ("description" in  response["items"][0]['snippet'].keys()) else None)
+        video_details["video_views"]         .append(video['statistics']['viewCount'] if ("viewCount" in  response["items"][0]['statistics'].keys()) else None)
+        video_details["video_likes"]         .append(video['statistics']['likeCount'] if ("likeCount" in  response["items"][0]['statistics'].keys()) else None)
+        video_details["video_duration"]      .append(video['contentDetails']['duration'] if ("duration" in  response["items"][0]['contentDetails'].keys()) else None)
+        try :
+            video_details["video_comments_count"].append(video['statistics'].get("commentCount","disabled") )#if ("commentCounts" in  response["items"][0]['statistics'].keys()) else None)
+        except:
+            # video_details["video_comments_count"]= video['statistics']["commentCount"] if ("commentCounts" in  response["items"][0]['statistics'].keys()) else None
+            video_details["video_comments_count"].append(0)
+        video_details["video_caption_status"].append(video['contentDetails']['caption'] if ("caption" in  response["items"][0]['contentDetails'].keys()) else None)
+        video_details["video_thumbnail"].append(video["snippet"]["thumbnails"]["standard"]["url"] if ("thumbnails" in  response["items"][0]['snippet'].keys()) else None)
+        # st.write(video_details["video_comments_count"])
+        if video_details["video_comments_count"][0] != "disabled":
+            try:
+                comments_response = youtube.commentThreads().list(
+                    part='snippet',
+                    videoId=video_id,
+                    maxResults=5
+                ).execute()
+                text = ""
+                for item in comments_response['items']:
+                    comment = item['snippet']['topLevelComment']['snippet']
+                    # st.write(f"- {comment['authorDisplayName']}: {comment['textDisplay'][:80]}...")
+                    text+=(comment['authorDisplayName']+"-->"+comment["textDisplay"]+"  \n   ")
+                video_details["video_commentText"].append(text)
+            except:
+                DATA_POOL['video_commentText'].append("NO COMMENTS")
+        else:
+            video_details['video_commentText'].append("NO COMMENTS")
         DATA_POOL['video_details'] = video_details
     else:
-        video_details["video_name"]          = None 
-        video_details["video_id"]            = None
-        video_details["video_date"]          = None
-        video_details["video_description"]   = None
-        video_details["video_views"]         = None
-        video_details["video_likes"]         = None
-        video_details["video_dislikes"]      = None
-        video_details["video_duration"]      = None
-        video_details["video_comments_count"]= None
-        video_details["video_caption_status"]= None
-        video_details["video_thumbnail"]     = None
+        video_details["video_name"]          = 0
+        video_details["video_id"]            = 0
+        video_details["video_date"]          = 0
+        video_details["video_description"]   = 0
+        video_details["video_views"]         = 0
+        video_details["video_likes"]         = 0
+        video_details["video_dislikes"]      = 0
+        video_details["video_duration"]      = 0
+        video_details["video_comments_count"]= 0
+        video_details["video_caption_status"]= 0
+        video_details["video_thumbnail"]     = 0
+        video_details["video_commentText"]   = 0
         DATA_POOL["video_details"] = video_details
     # st.write(video_id)
 
@@ -225,7 +223,6 @@ def get_channel_info(ch):
     channel_details = {
         "channel_id":[],
         "channel_name":[],
-        "channel_type" : [],
         "channel_views" : [],
         "channel_description" : [],
         "channel_videos":[],
@@ -243,7 +240,6 @@ def get_channel_info(ch):
         if any(response["items"]):
             channel_details["channel_id"].append(response['items'][0]["id"])
             channel_details["channel_name"].append(response['items'][0]["snippet"]["title"])
-            # channel_details["channel_type"].extend(response["items"][0]["topicDetails"]['topicCategories'])
             channel_details["channel_views"].append(response['items'][0]['statistics']['viewCount'])
             channel_details["channel_videos"].append(response['items'][0]['statistics']['videoCount'])
             channel_details["channel_subscriberCount"].append(response['items'][0]['statistics']['subscriberCount'])
@@ -258,7 +254,55 @@ def get_channel_info(ch):
     DATA_POOL["channel_details"] = channel_details
     return True
 
+
+def get_every_video_from_playlists(channel: str | None = None) -> pd.DataFrame:
+    '''
+    Gets every video details from the playlists.
+    
+    :TODO
+    this function depends on atleast one execution of the get_video_details() func. change soon
+    
+    '''
+    if ((channel == None) or (channel!=None)) and any(DATA_POOL['channel_details']):
+        allvid_df = pd.DataFrame(DATA_POOL["video_details"])
+        playlistData = list(DATA_POOL["playlist_details"]["playlist_id"])
+        for _playlist in playlistData:
+            # st.write(f"--{_playlist}")
+            get_playlistItems(_playlist,count=30)
+            playlistItemData =  list(DATA_POOL["playlist_items"]["item_id"])
+            pl =  list(DATA_POOL["playlist_items"]["item_name"])
+            a = dict(zip(pl,playlistItemData))
+            # st.write(a)
+            for _item in playlistItemData:
+                pass
+                # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                # st.write(f'{x} - {_item}')
+                # get_video_details(_item)
+                # t1 = pd.DataFrame(DATA_POOL["video_details"])
+                # allvid_df = allvid_df.merge(t1,how="outer")
+        return allvid_df
+    else:
+        get_channel_info(channel)
+        return get_every_video_from_playlists()
+
+def get_every_video_in_playlist(playlistId: str | None = None) -> pd.DataFrame:
+    """
+    returns DataFrame that consits of all videos info in a playlist
+    
+    :TODO
+    this function depends on atleast one execution of the get_video_details() func. change soon
+    
+    """
+
+    if any(sc_data:=DATA_POOL["video_details"]) and (playlistId != None):
+        video_data = pd.DataFrame(sc_data)
+        for pl_item in DATA_POOL["playlist_items"]["item_id"]:
+            get_video_details(pl_item)
+            temp_sc = pd.DataFrame(DATA_POOL["video_details"])
+            video_data = video_data.merge(temp_sc,how="outer")
+        # st.table(video_data)
+        return video_data
+
 if __name__ == '__main__':
-    # get_channel_id_by_username('Google')
     print("running main file")
-    wanted_data_gather()
+    
